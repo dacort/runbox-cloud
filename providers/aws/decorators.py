d437fcad3ea2f3f505b318de1e4bf1c2d648930c
Damon P. Cortesi <d.lifehacker@gmail.com>
2025-10-09 17:33:22 -0700

    remove old dirs

--- providers/aws/decorators.py
+++
@@ -1,8 +0,0 @@
-def depends_on(*resources):
-    """Class decorator to declare resource dependencies."""
-
-    def decorator(cls):
-        cls.depends_on = list(resources)
-        return cls
-
-    return decorator
--- providers/digitalocean/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import DigitalOcean, DigitalOceanResource
-from .compute import Droplet
-from .ssh import SSHKey
-
-__all__ = ["DigitalOcean", "DigitalOceanResource", "Droplet", "SSHKey"]
